39c3961a11c54a4fe4c4a81f90911d5c02e15f02
achrefmehri <ashmaymh@gmail.com>
2024-06-28 16:02:09 +0100

    handling sign up in front and back end with succ popup

--- front-end/src/components/pages/AddAnnoucement.jsx
+++ front-end/src/components/pages/AddAnnoucement.jsx
@@ -1,15 +1,16 @@
 
 import React, { useState } from 'react';
 import PopUp from '../PopUps/SignUpPop.jsx'
-const AddAnnouncement = () => {
+const AddAnnouncement = (props) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
+  const [location,setLocation]=useState("")
   const [image, setImage] = useState(null);
   const [price, setPrice] = useState(''); 
   const [error, setError] = useState('');
   const [allowed, setAllowed] = useState(false);
-
+  const [phoneNumber,setPhoneNumber]=useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,7 +24,7 @@ const AddAnnouncement = () => {
   return (
   
     <div className="flex justify-center items-center bg-gray-100 p-6" >
-    {!allowed && <PopUp allowed={setAllowed}/> }  
+    {allowed && <PopUp allowed={setAllowed}/> }  
       <div className="bg-white p-8 rounded-lg shadow-lg w-96 ">
         <div className="container p-4">
         <h2 className="text-2xl font-bold mb-6 text-center">Add Announcement</h2>
@@ -63,6 +64,39 @@ const AddAnnouncement = () => {
             </select>
           </div>
           <div className="mb-4">
+            <label htmlFor="location" className="block text-gray-700">Location:</label>
+            <select
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-xl bg-gray-100"
+            >
+              <option value="">Select Location</option>
+              <option value="Tunis">Tunis</option>
+              <option value="Sfax">Sfax</option>
+              <option value="Sousse">Sousse</option>
+              <option value="Kairouane">kairouane</option>
+              <option value="Gabes">Gabes</option>
+              <option value="Bizert">Bizert</option>
+              <option value="Ariana">Ariana</option>
+              <option value="Beja">Beja</option>
+              <option value="Monastir">Monastir</option>
+              <option value="Tataouine">Tataouine</option>
+              <option value="Hammamet">Hammamet</option>
+              <option value="Elkef">ElKef</option>
+              <option value="Ben Arous">Ben Arous</option>
+              <option value="Kasserine">Kasserine</option>
+              <option value="Gafsa">Gafsa</option>
+              <option value="Mahdia">Mahdia</option>
+              <option value="Mannouba">Mannouba</option>
+              <option value="Mednine">Mednine</option>
+              <option value="Nabel">Nabel</option>
+              <option value="Selyena">Selyena</option>
+              <option value="Tozer">Tozer</option>
+              <option value="Zaghouane">Zaghouan</option>
+              <option value="Sidi Bouzid">Sidi Bouzid</option>
+            </select>
+          </div>
+          <div className="mb-4">
             <label htmlFor="price" className="block text-gray-700">Price:</label>
             <input
               type="number"
@@ -76,13 +110,23 @@ const AddAnnouncement = () => {
             <label htmlFor="image" className="block text-gray-700">Image:</label>
             <input
               type="file"
-              onChange={""}
+              onChange={(e)=>setImage(e.target.files[0])}
               className="w-full p-2 border border-gray-300 rounded-xl bg-gray-100"
             />
           </div>
-          <button type="submit" className="w-full bg-[#ff385c] text-white p-2 rounded hover:bg-[#fe4869]">
+          <div className="mb-4">
+            <label htmlFor="Phone Number" className="block text-gray-700">Phone Number:</label>
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-xl bg-gray-100"
+              placeholder="Enter your Phone Number"
+            />
+          </div>
+          <span  type="submit" className="w-full bg-[#ff385c] text-white p-2 rounded hover:bg-[#fe4869]">
             Submit
-          </button>
+          </span>
 
         </form>
       </div>
