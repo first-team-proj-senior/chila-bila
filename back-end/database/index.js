09ad9178fe654a7f64cc9c17c06aad8bed5c2591
fathi777b <barhoumifathi2021@gmail.com>
2024-06-28 17:44:28 +0200

    add detaileAnnaucement component + style

--- back-end/database/index.js
+++ back-end/database/index.js
@@ -44,6 +44,5 @@ connection.connect()
  return connection.query(sql,[announce,id])
   }
 
-  // test 
 
   module.exports = {getAllAnnounces , saveAnnounceinDB , removeAnnounc , updateAnnounce , addUser , getAllusers}
